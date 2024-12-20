208ba977a7aa9c7855e0753a4ddac244d093f1ae
dittosamiii <samarthgupta2601@gmail.com>
2024-12-20 18:34:29 +0530

    unnecessary files deleted and configuration changed.

--- src/main/java/com/springboot/repository/RoleRepository.java
+++ src/main/java/com/springboot/repository/RoleRepository.java
@@ -1,6 +1,7 @@
 package com.springboot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.springboot.entity.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
